0a247941b39a5e9a6eb13f5894e8a22bf3ebb1ec
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-02-06 15:38:49 +0000

    working app

---
+++ app.js
@@ -0,0 +1,85 @@
+class App extends React.Component {
+    state = {
+        active: true,
+    }
+    handleSwitch = () => {
+        this.setState({
+            active: !this.state.active,
+        })
+    }
+    render() {
+        return (
+            <div>
+                <SwitchButton handleSwitch={this.handleSwitch} active={this.state.active} />
+                {this.state.active && <Timer />}
+            </div>
+        )
+    }
+}
+
+const SwitchButton = (props) => {
+
+    return(
+        <button onClick={props.handleSwitch} >{props.active ? "Turn off" : "Turn on" }</button>
+    )
+}
+
+
+
+
+class Timer extends React.Component {
+    intervalIndex = 0;
+    state = {
+        time: this.getTime(),
+    }
+
+    getTime() {
+        const time = new Date();
+        return ({
+            seconds: time.getSeconds(),
+            minutes: time.getMinutes(),
+            hours: time.getHours(),
+        })
+    }
+
+    setTime = () => {
+        const time = this.getTime();
+        this.setState({
+            time: time,
+        })
+    }
+
+    componentDidMount() {
+        this.intervalIndex = setInterval(this.setTime, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalIndex)
+    }
+
+
+    render() {
+        const { seconds, minutes, hours } = this.state.time;
+
+        return (
+            <div>{hours}:{minutes}:{seconds < 10 ? "0" + seconds : seconds}</div>
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
